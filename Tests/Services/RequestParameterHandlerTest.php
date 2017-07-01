dbfe9a7d68ef04fcfb4c93b7ab4a5c0a1ed6dbee
Shorinmaru <sho_fukamachi@hotmail.fr>
2017-07-02 01:20:59 +0200

    add unit test

--- Tests/Services/RequestParameterHandlerTest.php
+++ Tests/Services/RequestParameterHandlerTest.php
@@ -10,8 +10,36 @@
  */
 namespace EscapeHither\CrudManagerBundle\Tests\Services;
 use EscapeHither\CrudManagerBundle\Services\RequestParameterHandler;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RequestParameterHandlerTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function setUp()
+    {
+        $this->request = $this->getMockBuilder('Symfony\Component\HttpFoundation\RequestStack')->getMock();
+
+
+    }
+    public function testBuild(){
+        $attributes = [
+            "_controller" => "OpenMarketPlace\ProductManagerBundle\Controller\ProductController::indexAction",
+        ];
+        $requestParameterHandler = $this->buildRequest($attributes);
+        $requestParameterHandler->build();
+        $this->assertEquals('indexAction', $requestParameterHandler->getActionName());
+        $this->assertNotEmpty($requestParameterHandler->getAttributes());
+    }
+    
+    protected function buildRequest($attributes){
+        $request = new Request();
+        $requestStack = new RequestStack();
+        $container = $this->getMockBuilder(ContainerInterface::class)->getMock();
+        $request->initialize([],[],$attributes);
+        $requestStack->push($request);
+        return new RequestParameterHandler($requestStack,$container);
+    }
 
 }
